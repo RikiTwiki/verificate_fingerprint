2c3d50d2cbfd2bdd198b00d7f59225662acb5c73
rikitwiki <aibekrenadov@gmail.com>
2023-08-08 13:43:47 +0600

    api save db

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+
 plugins {
     id("org.springframework.boot") version "2.5.5"  // adjust the version if needed
     id("io.spring.dependency-management") version "1.0.11.RELEASE"  // adjust the version if needed
@@ -14,10 +20,16 @@ repositories {
 dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-data-rest")
     implementation ("org.springframework.boot:spring-boot-starter-web")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation ("org.hibernate:hibernate-core:5.6.3.Final") // or the version you are using
+    implementation ("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation ("org.xerial:sqlite-jdbc:3.42.0.0") // SQLite JDBC driver
     implementation ("org.springframework.boot:spring-boot-starter")
     implementation ("org.postgresql:postgresql:42.3.1") // replace with the latest version if needed
     implementation("ch.qos.logback:logback-classic:1.2.9") // Replace 1.2.9 with the latest version if needed
     implementation("com.machinezoo.sourceafis:sourceafis:3.14.0")
+    testImplementation ("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
